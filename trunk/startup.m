7ca4e6c7c702abfc87693e501ee08280ba6e8be1
ixentg@gmail.com <ixentg@gmail.com@a8e6913f-8765-f9e8-247b-8f534f9262ff>
2012-06-13 13:46:42 +0000

    matlab cvx config

---
+++ trunk/startup.m
@@ -0,0 +1,7 @@
+addpath /home/M04/acs/galpini/cvx/builtins
+addpath /home/M04/acs/galpini/cvx/commands
+addpath /home/M04/acs/galpini/cvx/functions
+addpath /home/M04/acs/galpini/cvx/lib
+addpath /home/M04/acs/galpini/cvx/structures
+addpath /home/M04/acs/galpini/cvx
+
