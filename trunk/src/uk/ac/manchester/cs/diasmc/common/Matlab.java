d53d521354cd52bf0268a2923e5dc25abe26fd1a
ixentg@gmail.com <ixentg@gmail.com@a8e6913f-8765-f9e8-247b-8f534f9262ff>
2012-06-14 13:27:42 +0000

    back to Runtime.exec, this time reading from stdout rather than stderr

--- trunk/src/uk/ac/manchester/cs/diasmc/common/Matlab.java
+++ trunk/src/uk/ac/manchester/cs/diasmc/common/Matlab.java
@@ -54,28 +54,29 @@ public class Matlab {
 		    final Runtime rt = Runtime.getRuntime();
 	
 		    String matlabexe = LocalSettings.getMatlabExe();
-		    String commandStr = matlabexe + " -nodesktop -nosplash -nodisplay " +
-    			"-r " + inputScriptName.replace(".m", "") + " -logfile " + outputFileName; 
+		    
+		    System.err.println(matlabexe +
+		    		//" -nodesktop -nosplash -nodisplay " +
+		    		"-r " + inputScriptName.replace(".m", "") + " -logfile " + outputFileName);
+		    
+		    final Process proc = rt.exec(new String[] { matlabexe,
+		    		//"-nodesktop", "-nosplash", "-nodisplay",
+		    		"-r", inputScriptName.replace(".m", ""),
+			    "-logfile", outputFileName}, null, new File(System.getProperty("user.dir"))); 
+		    //"-nodesktop", REMOVED FOR DEBUGGING - was before "-r"
 		    //older versions of matlab used /r amd /logfile
 		    
-		    System.err.println(commandStr);
-		    
-		    ProcessBuilder builder = new ProcessBuilder(commandStr);
-		    builder.redirectErrorStream(true);
-		    Process proc = builder.start();
-		    		    
 		    //The following lines make sure the Java app waits for the program to finish
-		    final InputStream stdout = proc.getInputStream();
-		    final InputStreamReader isr = new InputStreamReader(stdout);
+		    final InputStream stderr = proc.getInputStream();
+		    final InputStreamReader isr = new InputStreamReader(stderr);
 		    final BufferedReader br = new BufferedReader(isr);
 		    String line = null;
 	
 		    while ((line = br.readLine()) != null) {
 		    }
 		    final int exitVal = proc.waitFor();
-		} catch (Exception e) {
-			System.err.println("Unable to invoke solver");
-		    Utils.handleCriticalException(e);
+		} catch (final Throwable t) {
+		    Utils.handleCriticalException(new Exception("Unable to invoke solver"));
 		}
     }
 	
